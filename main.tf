a995c55c07883e2b66564588f8c3ba28272806e3
erfanpsss <erfanpsss@hotmail.com>
2023-05-12 19:39:08 +1000

    main.tf updated

--- main.tf
+++ main.tf
@@ -197,10 +197,11 @@ resource "aws_codebuild_project" "this" {
   }
 
   environment {
-    compute_type = "BUILD_GENERAL1_SMALL"
-    image        = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
-    type         = "LINUX_CONTAINER"
-
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:4.0"
+    type                        = "LINUX_CONTAINER"
+    privileged_mode             = true
+    image_pull_credentials_type = "CODEBUILD"
   }
 
   source {
