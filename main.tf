5259fc7f4615a9c08c8abe377f06f223f12a4f7b
erfanpsss <erfanpsss@hotmail.com>
2023-05-13 13:01:43 +1000

    main.tf updated

--- main.tf
+++ main.tf
@@ -132,6 +132,12 @@ resource "aws_security_group" "ecs_tasks_sg" {
   name        = "${var.infrustructure_name}${var.environment}_ecs_tasks_security_group"
   description = "Security group for ECS tasks"
   vpc_id      = module.vpc.vpc_id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 
@@ -191,7 +197,8 @@ resource "aws_ecs_service" "this" {
 
   network_configuration {
     subnets          = module.vpc.public_subnets
-    assign_public_ip = true
+    security_groups  = [aws_security_group.ecs_tasks_sg.id]
+    assign_public_ip = false
   }
 }
 
